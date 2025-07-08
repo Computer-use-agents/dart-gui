687754cad4a5ce40c7926352fa9d636231bb0c65
Kendrick-Stein <1227209738@qq.com>
2025-07-08 16:23:04 +0800

    testreward restore

--- tests/osworld/test_reward_.py
+++ tests/osworld/test_reward_.py
@@ -9,8 +9,13 @@ from verl.workers.reward_manager.osworld import OSWorldRewardManager
 def setup_env_vars(monkeypatch):
     """Automatically set up environment variables for all tests"""
     # Set environment variables for reward server
-    monkeypatch.setenv("REWARD_SERVER_URL", "https://sv-8cf0b533-6da2-423f-a6cb-4c737a630b4c-8000-x-aps-o-c4ef12a40d.sproxy.hd-01.alayanew.com:22443/v1")
-    monkeypatch.setenv("REWARD_MODEL", "qwen2.5_vl_7b")
+    monkeypatch.setenv("REWARD_SERVER_URL", "https://dashscope.aliyuncs.com/compatible-mode/v1")
+    monkeypatch.setenv("REWARD_MODEL", "qwen2.5-32b-instruct")
+    monkeypatch.setenv("REWARD_SERVER_API_KEY", "sk-075466341a4d4bf584ccb233c85cb6c1")
+    monkeypatch.setenv("WINDOW_SIZE", 5)
+    monkeypatch.setenv("STRIDE_SIZE", 1)
+    monkeypatch.setenv("N_COMPLETIONS", 4)
+    monkeypatch.setenv("IMAGE_NAME_CHECK", "FALSE")  # Set to "TRUE" to check image names in the dataset
     
     # You can add more OSWorld-specific environment variables if needed
     # monkeypatch.setenv("OSWORLD_SERVER_URL", "http://localhost:4999")
@@ -29,8 +34,9 @@ def test_reward():
     reward_server_url = os.getenv("REWARD_SERVER_URL")
     reward_model = os.getenv("REWARD_MODEL")
     
-    assert reward_server_url == "https://sv-8cf0b533-6da2-423f-a6cb-4c737a630b4c-8000-x-aps-o-c4ef12a40d.sproxy.hd-01.alayanew.com:22443/v1"
-    assert reward_model == "qwen2.5_vl_7b"
+    
+    # assert reward_server_url == "https://sv-8cf0b533-6da2-423f-a6cb-4c737a630b4c-8000-x-aps-o-c4ef12a40d.sproxy.hd-01.alayanew.com:22443/v1"
+    # assert reward_model == "qwen2.5_vl_7b"
     
     # Your test code here
     # You can now use these environment variables in your OSWorldRewardManager tests
@@ -42,5 +48,11 @@ def test_reward():
         "examples"
     )
     dataset_id = "osworld_trajectory"
-    score = manager.call_reward_model(dataset_id)
-    print("Final score:", score)
+    import time
+    start_time = time.time()
+    print("Start time:", start_time)
+    results = manager.call_reward_model(dataset_id)
+    print("End time:", time.time())
+    print("Time taken:", time.time() - start_time)  
+    print("Final results:", results)
+    # 每一个query 询问4次 做投票，耗时 167秒约 qwen32binstruct
